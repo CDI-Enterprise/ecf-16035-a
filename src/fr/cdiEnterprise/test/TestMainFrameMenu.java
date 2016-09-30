33c45645af1967aac93f1d962151bdb0b2cdc9a4
Klarhub <klarobskuro@mailoo.org>
2016-09-30 20:43:42 +0200

    30-09-2016 20:40
    PACKAGE VIEW
    Modification MainFrame
    - Mise en place modèle de base
    
    Création classe Menu
    - Construction JMenuBar pour MainFrame
    
    PACKAGE TEST
    - Création classe Test pour menu principal

---
+++ src/fr/cdiEnterprise/test/TestMainFrameMenu.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package fr.cdiEnterprise.test;
+
+import fr.cdiEnterprise.view.MainFrame;
+
+/**
+ * @author Claire
+ *
+ */
+public class TestMainFrameMenu {
+
+	/**
+	 * @param args
+	 */
+public static void main(String[] args) {
+		
+//		Window mainWindow = new Window();
+//		mainWindow.setVisible(true);
+		
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.setVisible(true);
+
+	}
+
+}
--- src/fr/cdiEnterprise/view/MainFrame.java
+++ src/fr/cdiEnterprise/view/MainFrame.java
@@ -6,9 +6,47 @@ package fr.cdiEnterprise.view;
 import javax.swing.JFrame;
 
 /**
+ * @version 1.0
  * @author Claire
  *
  */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
 public class MainFrame extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	//Get the contentPanel by default
+	JPanel panMain = (JPanel)this.getContentPane();
+	
+	//Default constructor
+	public MainFrame() {
+		
+		/* Window properties */
+		this.setTitle("CDI Enterprise - Recherche de stages et suivi des stagiaires");		
+		this.setResizable(true);
+		//Pop where the OS window usually does
+		this.setLocationByPlatform(true);
+		//MMC Close properties
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		/* Main content properties */
+		panMain.setPreferredSize(new Dimension (1024,600));
+		panMain.setBackground(Color.WHITE);
+		panMain.setLayout(new BorderLayout(20,20));
+	
+		/* Main menu */
+		this.setJMenuBar(new Menu());
+		
+		this.pack();
+		this.setLocationRelativeTo(null);
+				
+	}
 }
