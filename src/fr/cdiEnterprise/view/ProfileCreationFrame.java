e8b36e3f3036273541d74f203f3930e5a106bc38
Klarhub <klarobskuro@mailoo.org>
2016-10-02 21:33:01 +0200

    02-10-16 21:32
    PACKAGE VIEW
    Modifications majeures ProfileCreationFrame

--- src/fr/cdiEnterprise/view/ProfileCreationFrame.java
+++ src/fr/cdiEnterprise/view/ProfileCreationFrame.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
@@ -30,41 +31,48 @@ public class ProfileCreationFrame extends MainFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	//TODO lists JComboBox
+	String [] company = {"Aucune", "9e compagnie", "Cie", "Comme Pagny", "Autre..."};
+	
 	/**
 	 * 
 	 */
 	public ProfileCreationFrame() {
 		super();
-		//Override title from MainFrame
+		//Override frame title
+		this.setTitle("CDI Enterprise - Création de profil");
 		
-		/* Header with one title */
+		/* Header */
 		JPanel panNorth = new JPanel();
 		panMain.add(panNorth, BorderLayout.NORTH);
-		panNorth.setLayout(new MigLayout());
-		panNorth.add(new JLabel("CRÉATION DE PROFIL"), "center, wrap 15");
-		panNorth.add(new JLabel("Les champs marqués d'étoiles sont obligatoires pour : "
+		panNorth.setLayout(new FlowLayout());
+		//Main information on compulsory fields
+		JLabel lblFieldInfo = new JLabel("Les champs marqués d'étoiles sont obligatoires pour : "
 				+ "* tous les utilisateurs / "
 				+ "** les statuts Stagiaire et Ancien / "
-				+ "*** le statut Ancien"));
-		panNorth.setBorder(BorderFactory.createLineBorder(Color.WHITE));	
+				+ "*** le statut Ancien");
+		lblFieldInfo.setFont(new Font(getName(), Font.BOLD, 14));
+		panNorth.add(lblFieldInfo);
+		panNorth.setBorder(BorderFactory.createLineBorder(Color.RED));	
+		
+	
 		
 		/* Body */
 		//Main center panel with four horizontal parts
 		JPanel panCenter = new JPanel();
 		panMain.add(panCenter, BorderLayout.CENTER);
-		panCenter.setLayout(new GridLayout(4,1));
-		
-		//Second horizontal part: first log-in informations
-		JPanel panCenterRegister = new JPanel();
-		panCenter.add(panCenterRegister);
-		panCenterRegister.setLayout(new MigLayout());
-		panCenterRegister.setBorder(BorderFactory.createTitledBorder("ENREGISTREMENT"));
+		panCenter.setLayout(new MigLayout());
 	
 		
-		//panCenterRegister.add(new JLabel("Les champs marquées d'un * sont obligatoires pour tous."), "wrap");
+		//FIRST horizontal part: first log-in informations
+		JPanel panCenterRegister = new JPanel();
+		panCenter.add(panCenterRegister, "wrap, w 1425!");
+		panCenterRegister.setLayout(new MigLayout());
+		panCenterRegister.setBorder(BorderFactory.createTitledBorder("ENREGISTREMENT"));
 		
 		//User alias
-		panCenterRegister.add(new JLabel("Nom d'utilisateur* : "), "w 150!");
+		JLabel lblAlias = new JLabel("Nom d'utilisateur* : ");
+		panCenterRegister.add(lblAlias, "w 200!");
 		panCenterRegister.add(new JTextField(20));
 		panCenterRegister.add(new JLabel("<html><body><i>Maximum 20 caractères</i></body></html>"), "gapleft20, wrap");
 		
@@ -73,7 +81,7 @@ public class ProfileCreationFrame extends MainFrame {
 		panCenterRegister.add(new JTextField(20));
 		panCenterRegister.add(new JLabel("<html><body><i>Ne sera pas rendu public</i></body></html>"), "gapleft20, wrap");
 		
-		//User status TODO One selection possible
+		//User status TODO one status possible
 		panCenterRegister.add(new JLabel("Statut* : "));
 		panCenterRegister.add(new JRadioButton("Stagiaire"));
 		panCenterRegister.add(new JRadioButton("Ancien"));
@@ -85,41 +93,105 @@ public class ProfileCreationFrame extends MainFrame {
 		
 		//User trainer
 		panCenterRegister.add(new JLabel("Nom du formateur** : "));
-		panCenterRegister.add(new JTextField(20), "wrap");
+		panCenterRegister.add(new JTextField(20));
 		
-		//Third horizontal part: public profile informations
+		
+		//SECOND horizontal part: public profile informations
 		JPanel panCenterPublic = new JPanel();
-		panCenter.add(panCenterPublic);
+		panCenter.add(panCenterPublic, "wrap, w 1425!");
 		panCenterPublic.setLayout(new MigLayout());
 		panCenterPublic.setBorder(BorderFactory.createTitledBorder("PROFIL PUBLIC"));
 		
 		//Code of training session, usually 5 numbers
-		panCenterPublic.add(new JLabel("Numéro de session** : "), "w 150!");
+		panCenterPublic.add(new JLabel("Numéro de session** : "), "w 200!");
 		panCenterPublic.add(new JTextField(20), "wrap");
 		
 		//Main programming language of the training
 		panCenterPublic.add(new JLabel("Langage principal** : "));
-		String [] language = {"C#", "Java"};
-		JComboBox<String> cboLanguage = new JComboBox<String> ();
-		for (int i = 0; i<language.length; i++) {
-			cboLanguage.addItem(language[i]);
+		panCenterPublic.add(new JRadioButton("C#"));
+		panCenterPublic.add(new JRadioButton("Java"), "wrap");
+		
+		//List of approached companies
+		panCenterPublic.add(new JLabel("Entreprise(s) démarchée(s)*** : "));
+		JComboBox<String> approachedCie = new JComboBox<String> ();
+		for (int i = 0; i<company.length; i++) {
+			approachedCie.addItem(company[i]);
 		}
-		cboLanguage.setMaximumRowCount(2);
-		panCenterPublic.add(cboLanguage);
+		approachedCie.setMaximumRowCount(5);
+		panCenterPublic.add(approachedCie, "wrap");
 		
-		//
+		//Current profession
+		panCenterPublic.add(new JLabel("Profession actuelle*** : "));
+		panCenterPublic.add(new JTextField(20), "wrap");
 		
-		//Fourth horizontal part: protected profile informations
+		//Current company
+		panCenterPublic.add(new JLabel("Entreprise actuelle*** : "));
+		JComboBox<String> currentCie = new JComboBox<String> ();
+		for (int i = 0; i<company.length; i++) {
+			currentCie.addItem(company[i]);
+		}
+		currentCie.setMaximumRowCount(5);
+		panCenterPublic.add(currentCie, "wrap");
+		
+		//Former company
+		panCenterPublic.add(new JLabel("Ancienne(s) entreprise(s)*** : "));
+		JComboBox<String> formerCie = new JComboBox<String> ();
+		for (int i = 0; i<company.length; i++) {
+			formerCie.addItem(company[i]);
+		}
+		formerCie.setMaximumRowCount(5);
+		panCenterPublic.add(formerCie, "wrap");
+		
+		
+		//THIRD horizontal part: protected profile informations
 		JPanel panCenterProtected = new JPanel();
-		panCenter.add(panCenterProtected);
+		panCenter.add(panCenterProtected, "wrap, w 1425!");
 		panCenterProtected.setLayout(new MigLayout());
 		panCenterProtected.setBorder(BorderFactory.createTitledBorder("PROFIL RESTREINT"));
 		
-		//Fifth horizontal part: optional profile informations
+		//Trainee surname for trainer (monitoring)
+		panCenterProtected.add(new JLabel("Nom** : "), "w 200!");
+		panCenterProtected.add(new JTextField(20), "wrap");
+		
+		//Trainee given name for trainer (monitoring)
+		panCenterProtected.add(new JLabel("Prénom** : "));
+		panCenterProtected.add(new JTextField(20), "wrap");
+		
+		
+		//FOURTH horizontal part: optional profile informations
 		JPanel panCenterOptional = new JPanel();
-		panCenter.add(panCenterOptional);
+		panCenter.add(panCenterOptional, "w 1425!");
 		panCenterOptional.setLayout(new MigLayout());
 		panCenterOptional.setBorder(BorderFactory.createTitledBorder("VISIBLE PUBLIQUEMENT SI RENSEIGNÉ"));
+	
+		//Other known programming languages
+		panCenterOptional.add(new JLabel("Autre(s) langage(s) : "), "w 200!");
+		String [] progLanguage = {"C", "PHP"};
+		JComboBox<String> cboProgLanguage = new JComboBox<String> ();
+		for (int i = 0; i<progLanguage.length; i++) {
+			cboProgLanguage.addItem(progLanguage[i]);
+		}
+		cboProgLanguage.setMaximumRowCount(2);
+		panCenterOptional.add(cboProgLanguage, "wrap");
+		
+		//Known graphic API
+		panCenterOptional.add(new JLabel("API graphique : "), "w 200!");
+		String [] graphicApi = {"AWT", "JFace", "Swing", "SWT"};
+		JComboBox<String> cboGraphicApi = new JComboBox<String> ();
+		for (int i = 0; i<graphicApi.length; i++) {
+			cboGraphicApi.addItem(graphicApi[i]);
+		}
+		cboGraphicApi.setMaximumRowCount(4);
+		panCenterOptional.add(cboGraphicApi, "wrap");
+		
+		//User website
+		panCenterOptional.add(new JLabel("Site Internet : "), "w 200!");
+		panCenterOptional.add(new JTextField(20), "wrap");
+		
+		//User LinkedIn
+		panCenterOptional.add(new JLabel("Profil LinkedIn"));
+		panCenterOptional.add(new JTextField(20), "wrap");
+		
 		
 		//Footer with two buttons
 		JPanel panSouth = new JPanel();
