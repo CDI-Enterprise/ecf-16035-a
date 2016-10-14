8244756fe20c3a8788a70eca4b7b452629981241
Klarhub <klarobskuro@mailoo.org>
2016-10-14 10:10:07 +0200

    14-10-2016 10:09
    PACKAGE CONTROL
    Modificaiton PanelUserListeners

--- src/fr/cdiEnterprise/dao/Datas.java
+++ src/fr/cdiEnterprise/dao/Datas.java
@@ -4,6 +4,7 @@
 package fr.cdiEnterprise.dao;
 import fr.cdiEnterprise.control.MpClient;
 import fr.cdiEnterprise.model.Department;
+import fr.cdiEnterprise.model.FormerTrainee;
 import fr.cdiEnterprise.model.Language;
 import fr.cdiEnterprise.model.Region;
 import fr.cdiEnterprise.model.Trainee;
@@ -18,21 +19,22 @@ import fr.cdiEnterprise.service.Regions;
 import fr.cdiEnterprise.service.Users;
 
 /**
- * @author Claire, Anaïs
+ * @author Claire, Anaïs, Nicolas
  *
  */
 public class Datas {
 
-	private static Languages progLangList;
-	
 	private static Users usersList;
-	private static Server exchange;
+	
+	private static Companies companiesList = new Companies();
+	
 	private static Clients clientBox;
+	private static Server exchange;
+	
 	private static Departments departmentsList = new Departments();
 	private static Regions regionsList = new Regions();
-	private static Companies companiesList = new Companies();
+
 	private static Languages languagesCompanyList = new Languages();
-	private static Departments listeDepartments = new Departments();
 
 	/**
 	 * 
@@ -43,50 +45,27 @@ public class Datas {
 
 	public static void init(){
 		
-		// Test du server de messagerie
-		exchange = new Server();
-		clientBox = new Clients();
-
-		// Non exhaustive DB programming language
-		progLangList = new Languages();
-		progLangList.add(new Language("C"));
-		progLangList.add(new Language("C++"));
-		progLangList.add(new Language("Delphi"));
-		progLangList.add(new Language("JavaScript"));
-		progLangList.add(new Language("Perl"));
-		progLangList.add(new Language("PHP"));
-		progLangList.add(new Language("Python"));
-		progLangList.add(new Language("R"));
-		progLangList.add(new Language("Ruby"));
-		progLangList.add(new Language("Visual Basic"));
-		
-		// Test list trainee
-		usersList = new Users();
-
-//		usersList.add(new Trainee("email@boite.fr", "Pseudo1", "mdp", "Stagiaire", "Saint-Jérôme", "D. Muller", "Nom1", "Prénom1",
-//				"16035", null, "Java", "Swing", "site.fr", "LI"));
-		
-// HEAD
-		// List Department
-		//Departments allDepartments = new Departments();
-
+		// User database
 		usersList = new Users();
 		usersList.add(new Trainer("01-01-2010 08:00", "Formateur", "Domi", "domim@afpa.fr", "Saint-Jérôme"));
+		usersList.add(new FormerTrainee("29-08-2015 08:00", "Ancien", "CDI", "cdi@mail.org", "Saint-Jérôme", "E. Cattaneo"));
+		
 		usersList.add(new Trainee("29-08-2016 08:00", "Stagiaire", "Klaroo", "klaroo@mail.fr", "Saint-Jérôme", "D. Muller"));
 		usersList.add(new Trainee("29-08-2016 08:00", "Stagiaire", "Cookie", "cookie@mail.fr", "Saint-Jérôme", "D. Muller"));
 		usersList.add(new Trainee("29-08-2016 08:00", "Stagiaire", "Omy", "omy@mail.fr", "Saint-Jérôme", "D. Muller"));
 		usersList.add(new Trainee("29-08-2016 08:00", "Stagiaire", "Oracle", "oracle@mail.fr", "Saint-Jérôme", "D. Muller"));
 		usersList.add(new Trainee("29-08-2016 08:00", "Stagiaire", "Dark Swan", "darkswan@mail.fr", "Saint-Jérôme", "D. Muller"));
-
-		// Creation des boite de Messagerie
 		
+		// Test du server de messagerie
+		exchange = new Server();
+		clientBox = new Clients();
+		
+		// Creation des boite de Messagerie
 		for(User current : usersList) {
 			MpClient client = new MpClient(exchange, current.getAlias());
 			clientBox.add(client);
 		}
 		
-
-		// remplir les thèmes de la biblio
 
 		//Departments allStaticDepartment = new Departments();
 		for(int i = 0; i < Department.DEPARTMENTS.length; i++) {
@@ -104,9 +83,7 @@ public class Datas {
 		}
 	}
 	
-	public static Departments getDepartmentsList() {
-		return departmentsList;
-	}
+	
 	/**
 	 * @return the usersList
 	 */
@@ -114,10 +91,22 @@ public class Datas {
 		return usersList;
 	}
 	
-	public static Departments getListeDepartments() {
-		return listeDepartments;
+	public static Companies getCompaniesList() {
+		return companiesList;
 	}
 
+	public static void setCompaniesList(Companies listeCompanies) {
+		Datas.companiesList = listeCompanies;
+	}
+	
+	public static Clients getClientBox() {
+		return clientBox;
+	}
+	
+	public static Departments getDepartmentsList() {
+		return departmentsList;
+	}
+	
 	public static void setDepartmentsList(Departments listeDepartments) {
 		Datas.departmentsList = listeDepartments;
 	}
@@ -130,20 +119,8 @@ public class Datas {
 		Datas.regionsList = listeRegions;
 	}
 
-	public static Companies getCompaniesList() {
-		return companiesList;
-	}
-
-	public static void setCompaniesList(Companies listeCompanies) {
-		Datas.companiesList = listeCompanies;
-	}
-
 	public static Languages getLanguagesCompanyList() {
 		return languagesCompanyList;
-	}
-
-	public static Clients getClientBox() {
-		return clientBox;
 	}
 
 }
