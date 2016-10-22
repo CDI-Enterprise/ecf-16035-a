5cfd9b54b165bbb54a609810178b41bc92d75e40
Klarhub <klarobskuro@mailoo.org>
2016-10-22 21:25:21 +0200

    Tried connection in main app without success

--- src/fr/cdiEnterprise/application/Application.java
+++ src/fr/cdiEnterprise/application/Application.java
@@ -1,11 +1,13 @@
 package fr.cdiEnterprise.application;
 
+import java.sql.SQLException;
+
 import fr.cdiEnterprise.dao.OldDatas;
 import fr.cdiEnterprise.view.MainFrame;
 
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		OldDatas.init();
 		
