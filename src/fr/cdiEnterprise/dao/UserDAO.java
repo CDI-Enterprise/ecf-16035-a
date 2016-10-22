0ae64aed0b9d87f579b33459b15a7eecf579b872
Klarhub <klarobskuro@mailoo.org>
2016-10-22 15:12:45 +0200

    Create two SQL request: create, delete

--- src/fr/cdiEnterprise/dao/UserDAO.java
+++ src/fr/cdiEnterprise/dao/UserDAO.java
@@ -3,17 +3,139 @@
  */
 package fr.cdiEnterprise.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 /**
  * @author Claire
  *
  */
 public class UserDAO {
 
+
+	private Connection connect;
+
+	// Prepared statement for SQL request
+	private static PreparedStatement createUser;
+	private static PreparedStatement deleteUser;
+
+	private static int result;
+	private static ResultSet requestRes;
+
+
 	/**
-	 * 
+	 * Asks for the connection to DB.
 	 */
 	public UserDAO() {
-		// TODO Auto-generated constructor stub
+		this.connect = DBConnection.getConnect();
+	}
+	
+
+	/**
+	 * Insert a new user in database.
+	 * @param id
+	 * @param date
+	 * @param status
+	 * @param alias
+	 * @param email
+	 * @param afpa
+	 * @return creationDone (test code)
+	 * @throws SQLException
+	 */
+	public String create(int id, String date, String status, String alias, String email, String afpa) throws SQLException {
+
+		result = 0;
+
+		try {
+			createUser = connect.prepareStatement("INSERT INTO cdi_user VALUES (?, ?, ?, ?, ?, ?)");
+			createUser.setInt(1, id);
+			createUser.setString(2, date);
+			createUser.setString(3, status);
+			createUser.setString(4, alias);
+			createUser.setString(5, email);
+			createUser.setString(6, afpa);
+			result = createUser.executeUpdate();
+		} catch (SQLException e) {
+			//			e.printStackTrace();
+			System.err.println("Requête incorrecte : l'utilisateur n'a pas pu être créé.");
+		}
+
+		finally {
+			closeRequest(createUser);
+		}
+		// Test code
+		String creationDone = null;
+
+		if (result == 0) {
+			creationDone = "Aucune création effectuée.";
+		}
+		else {
+			creationDone = result + " création(s) effectuée(s).";
+		}
+
+		return creationDone;
+		// Fin test code
 	}
 
+	/**
+	 * Deletes an user in database.
+	 * @param id
+	 * @return deleteDone (test code)
+	 * @throws SQLException
+	 */
+	public String delete(int id) throws SQLException {
+
+		result = 0;
+
+		try {
+			deleteUser = connect.prepareStatement("DELETE FROM cdi_user WHERE user_id = ?");
+			deleteUser.setInt(1,id);
+			result = deleteUser.executeUpdate();
+		} catch (SQLException e) {
+			//			e.printStackTrace();
+			System.err.println("Requête incorrecte : l'utilisateur n'a pas pu être supprimé.");
+		}
+
+		finally {
+			closeRequest(deleteUser);
+		}
+		// Test code
+		String deleteDone = null;
+
+		if (result == 0) {
+			deleteDone = "Aucune suppression effecutée."; // Test code
+		}
+		else {
+			deleteDone = result + " suppression(s) effectuée(s)."; // Test code
+		}
+
+		return deleteDone;
+		// Fin test code
+	}
+
+	/**
+	 * Closes request and automatically result of request if there's one.
+	 * @param prepStatmt
+	 * @return a string (test code)
+	 * @throws SQLException
+	 */
+	private void closeRequest(PreparedStatement prepStatmt) throws SQLException {
+		if (prepStatmt != null) {
+			prepStatmt.close();
+			// Test code
+			System.out.println("Requête terminée.");
+			boolean request = false;
+			request = prepStatmt.isClosed();
+			System.out.println("Statement is closed: " + request);
+			if (requestRes != null) {
+				boolean result = false;
+				result = requestRes.isClosed();
+				System.out.println("Result of request is closed: " + result);
+			}
+			// Fin test code
+		}	
+	}
 }
--- src/fr/cdiEnterprise/dao/test/UserDAOTest.java
+++ src/fr/cdiEnterprise/dao/test/UserDAOTest.java
@@ -3,17 +3,39 @@
  */
 package fr.cdiEnterprise.dao.test;
 
+import java.sql.SQLException;
+
+import fr.cdiEnterprise.dao.UserDAO;
+
 /**
  * @author Claire
  *
  */
 public class UserDAOTest {
 
-	/**
-	 * 
-	 */
-	public UserDAOTest() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) throws SQLException {
+
+		UserDAO userDAO = new UserDAO();
+		
+		// Test search a user
+//		String test = userDAO.search(3);
+//		System.out.println("App : " + test);
+		
+		// Test create a user
+		String creationDone = userDAO.create(10, "22/10/2016", "Ancien", "CDI", "cdi@mail.com", "Saint-Jérôme");
+		System.out.println(creationDone);
+		
+		// Test read all users
+//		Users users = new Users();
+//		users = userDAO.read();
+//		System.out.println("App : " + users);
+//		
+		// Test update a user
+		
+		// Test delete a user
+//		String deleteDone = userDAO.delete(10);
+//		System.out.println(deleteDone);
+		
 	}
 
 }
