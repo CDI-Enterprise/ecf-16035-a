00566f8cbbd8a95c161a389e6eaaa5abbaf002f8
Klarhub <klarobskuro@mailoo.org>
2016-10-27 08:54:40 +0200

    Create a NoButtonException

---
+++ src/fr/cdiEnterprise/exceptions/NoButtonException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package fr.cdiEnterprise.exceptions;
+
+/**
+ * @author Claire
+ *
+ */
+public class NoButtonException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoButtonException(String message) {
+		super(message);
+	}
+	
+}
